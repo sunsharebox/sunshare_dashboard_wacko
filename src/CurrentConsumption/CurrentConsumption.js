c88797d4319c8305ea808a52d8a4d50dd0bb5d7d
sunshare <49123814+sunsharebox@users.noreply.github.com>
2019-08-29 00:46:30 +0200

    Add files via upload

---
+++ src/CurrentConsumption/CurrentConsumption.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function CurrentConsumption() {
+  return (
+    <div className="App">
+      <header className="App-header">
+   
+      </header>
+    </div>
+  );
+}
+
+export default CurrentConsumption;
